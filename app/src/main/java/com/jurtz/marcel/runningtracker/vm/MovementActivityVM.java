d4897e885bc24bf5ed8d48966d184eb8e93aa317
MarcelJurtz <jurtzmarcel@gmail.com>
2018-02-07 15:05:08 +0100

    Add handler for updating time and observable properties

--- app/src/main/java/com/jurtz/marcel/runningtracker/vm/MovementActivityVM.java
+++ app/src/main/java/com/jurtz/marcel/runningtracker/vm/MovementActivityVM.java
@@ -1,31 +1,84 @@
 package com.jurtz.marcel.runningtracker.vm;
 
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+import android.databinding.Observable;
+import android.databinding.PropertyChangeRegistry;
 import android.view.View;
 
+import com.jurtz.marcel.runningtracker.BR;
 import com.jurtz.marcel.runningtracker.ICustomActivity;
-import com.jurtz.marcel.runningtracker.MovementActivity;
 import com.jurtz.marcel.runningtracker.database.AppDatabase;
+import com.jurtz.marcel.runningtracker.helper.TimeFormatter;
 import com.jurtz.marcel.runningtracker.model.Movement;
-
-import java.time.LocalDate;
-import java.time.Period;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
+import android.os.Handler;
 
-public class MovementActivityVM {
+public class MovementActivityVM implements Observable {
 
     ICustomActivity view;
     private Date movementStarted;
+    private Handler handler;
+    private PropertyChangeRegistry registry = new PropertyChangeRegistry();
+
+    private int seconds;
+
+    private String timerText;
+    @Bindable
+    public String getTimerText() {
+        return timerText;
+    }
+    private void setTimerText(String timerText) {
+        this.timerText = timerText;
+        registry.notifyChange(this, BR.timerText);
+    }
+
+    private String distanceText;
+    @Bindable
+    public String getDistanceText() {
+        return distanceText;
+    }
+    private void setDistanceText(String distanceText) {
+        this.distanceText = distanceText;
+        registry.notifyChange(this, BR.distanceText);
+    }
+
     public MovementActivityVM(ICustomActivity view) {
         this.view = view;
         movementStarted = new Date();
+        seconds = 0;
+        handler = new Handler();
+        timerText = "00:00:00";
+        distanceText = "0.00 KM";
+        startMovement();
     }
 
-    public String title = "MOVE";
-    public String timerText = "00:00:00";
-    public String distanceText = "0.00 KM";
+    @Override
+    public void addOnPropertyChangedCallback(OnPropertyChangedCallback onPropertyChangedCallback) {
+        registry.add(onPropertyChangedCallback);
+    }
+
+    @Override
+    public void removeOnPropertyChangedCallback(OnPropertyChangedCallback onPropertyChangedCallback) {
+        registry.remove(onPropertyChangedCallback);
+    }
+
+    private Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            setTimerText(TimeFormatter.getFormattedTime(++seconds));
+            handler.postDelayed(runnable, 1000);
+        }
+    };
+
+    private void startMovement() {
+        handler.postDelayed(runnable, 1000);
+    }
 
     public void onCmdStopClick(View v) {
+
+        // TODO TEST FOR DATABASE
         Movement movement = new Movement();
         movement.timeStamp = movementStarted;
         movement.distance = 1500;
@@ -45,4 +98,6 @@ public class MovementActivityVM {
 
         view.returnToPreviousActivity();
     }
+
+
 }
